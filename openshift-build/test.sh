693f0eb9f00f6b398da7b0e909a2773d2d69d930
Maru Newby <marun@redhat.com>
2020-06-03 21:56:11 -0700

    Ensure test and verify use recent bash

---
+++ openshift-build/test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
+
+# `make verify` requires recent bash (>= 4.3). If the system bash is
+# not recent (e.g openshift ci and macos), download and compile a
+# newer bash and make it available in the path.
+RECENT_BASH="$( os::recent_bash )"
+WHICH_BASH="$( which bash )"
+if [[ "${RECENT_BASH}" != "${WHICH_BASH}" ]]; then
+  BASH_PATH="$( dirname "${RECENT_BASH}" )"
+  export PATH="${BASH_PATH}:${PATH}"
+fi
+
+mkdir -p "${BASETMPDIR}/test-artifacts"
+KUBE_JUNIT_REPORT_DIR="${BASETMPDIR}/test-artifacts" hack/make-rules/test.sh
