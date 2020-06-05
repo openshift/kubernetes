a906d4fc49d0098a34586e107fdced26ab6eccbe
Maru Newby <marun@redhat.com>
2020-06-04 19:44:48 -0700

    Keep fixing

--- openshift-build/test.sh
+++ openshift-build/test.sh
@@ -5,9 +5,9 @@ source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 # Upstream testing requires recent bash (>= 4.3). If the system bash
 # is not recent (e.g openshift ci and macos), download and compile a
 # newer bash and make it available in the path.
-PATH="$( os::path_with_recent_bash )"
+export PATH="$( os::deps::path_with_recent_bash )"
 
-bash --version
+/usr/bin/env bash --version
 
 mkdir -p "${BASETMPDIR}/test-artifacts"
 KUBE_JUNIT_REPORT_DIR="${BASETMPDIR}/test-artifacts" hack/make-rules/test.sh
--- openshift-build/verify.sh
+++ openshift-build/verify.sh
@@ -5,18 +5,19 @@ source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 # Upstream verify requires recent bash (>= 4.3). If the system bash is
 # not recent (e.g openshift ci and macos), download and compile a
 # newer bash and make it available in the path.
-PATH="$( os::deps::path_with_recent_bash )"
+export PATH="$( os::deps::path_with_recent_bash )"
 
-bash --version
+/usr/bin/env bash --version
 
 # Upstream verify requires protoc (>= 3.0.0). If not present, download
 # a recent version and make it available in the path.
-PATH="$( os::deps::path_with_protoc )"
+export PATH="$( os::deps::path_with_protoc )"
 
 # Attempt to verify without docker if it is not available.
-OS_RUN_WITHOUT_DOCKER=
-if ! which docker > /dev/null; then
-  OS_RUN_WITHOUT_DOCKER=y
+export OS_RUN_WITHOUT_DOCKER=
+if ! which docker &> /dev/null; then
+  os::log::warning "docker not available, attempting to run verify without it"
+  export OS_RUN_WITHOUT_DOCKER=y
 fi
 
 hack/make-rules/verify.sh
