b835b6d01f406bc6aefbf99ecf24152581f6ee2a
Maru Newby <marun@redhat.com>
2020-06-04 19:32:01 -0700

    Keep fixing test and verify

--- openshift-build/verify.sh
+++ openshift-build/verify.sh
@@ -2,16 +2,21 @@
 
 source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 
-# `make verify` requires recent bash (>= 4.3). If the system bash is
+# Upstream verify requires recent bash (>= 4.3). If the system bash is
 # not recent (e.g openshift ci and macos), download and compile a
 # newer bash and make it available in the path.
-RECENT_BASH="$( os::recent_bash )"
-WHICH_BASH="$( which bash )"
-if [[ "${RECENT_BASH}" != "${WHICH_BASH}" ]]; then
-  BASH_PATH="$( dirname "${RECENT_BASH}" )"
-  export PATH="${BASH_PATH}:${PATH}"
-fi
+PATH="$( os::deps::path_with_recent_bash )"
 
 bash --version
+
+# Upstream verify requires protoc (>= 3.0.0). If not present, download
+# a recent version and make it available in the path.
+PATH="$( os::deps::path_with_protoc )"
+
+# Attempt to verify without docker if it is not available.
+OS_RUN_WITHOUT_DOCKER=
+if ! which docker > /dev/null; then
+  OS_RUN_WITHOUT_DOCKER=y
+fi
 
 hack/make-rules/verify.sh
