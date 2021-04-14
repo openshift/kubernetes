e22e27348917ad84e09b5c96b58b7dc241c064c4
Jan Safranek <jsafrane@redhat.com>
2021-04-14 17:19:37 +0200

    vSphere: Return useful errors from parameter validation
    Returning "VolumeOptions verification failed" when StorageClass parameter
    validation failed is not really useful. It does not tell which parameter is
    wrong and what is a VolumeOption at all.
    
    Trying to return something more useful here.

--- staging/src/k8s.io/legacy-cloud-providers/vsphere/vclib/custom_errors.go
+++ staging/src/k8s.io/legacy-cloud-providers/vsphere/vclib/custom_errors.go
@@ -24,7 +24,6 @@ const (
 	NoDiskUUIDFoundErrMsg      = "No disk UUID found"
 	NoDevicesFoundErrMsg       = "No devices found"
 	DiskNotFoundErrMsg         = "No vSphere disk ID found"
-	InvalidVolumeOptionsErrMsg = "VolumeOptions verification failed"
 	NoVMFoundErrMsg            = "No VM found"
 )
 
@@ -34,6 +33,5 @@ var (
 	ErrNoDiskUUIDFound      = errors.New(NoDiskUUIDFoundErrMsg)
 	ErrNoDevicesFound       = errors.New(NoDevicesFoundErrMsg)
 	ErrNoDiskIDFound        = errors.New(DiskNotFoundErrMsg)
-	ErrInvalidVolumeOptions = errors.New(InvalidVolumeOptionsErrMsg)
 	ErrNoVMFound            = errors.New(NoVMFoundErrMsg)
 )
