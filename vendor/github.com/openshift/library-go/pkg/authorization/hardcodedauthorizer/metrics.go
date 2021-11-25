7a00585772d7e84c2f1f5a70c630937b6706a000
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2021-11-25 13:46:54 +0100

    log hardcoded authorizer results

--- vendor/github.com/openshift/library-go/pkg/authorization/hardcodedauthorizer/metrics.go
+++ vendor/github.com/openshift/library-go/pkg/authorization/hardcodedauthorizer/metrics.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"k8s.io/apiserver/pkg/authorization/authorizer"
+	"k8s.io/klog/v2"
 )
 
 type metricsAuthorizer struct{}
@@ -20,6 +21,11 @@ type metricsAuthorizer struct{}
 // IsResourceRequest() bool - checked
 // GetPath() string - checked
 func (metricsAuthorizer) Authorize(ctx context.Context, a authorizer.Attributes) (authorized authorizer.Decision, reason string, err error) {
+	klog.V(2).Infof("attrs %+v user info %+v", a, a.GetUser())
+	defer func() {
+		klog.V(2).Infof("%+v %v", authorized, reason)
+	}()
+
 	if a.GetUser().GetName() != "system:serviceaccount:openshift-monitoring:prometheus-k8s" {
 		return authorizer.DecisionNoOpinion, "", nil
 	}
