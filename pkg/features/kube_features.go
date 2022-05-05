f207a2069157b4cda32627e17a34bb9c8e4427e3
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2022-05-05 15:10:31 +0200

    UPSTREAM: 108309: pkg/features: disable LegacyServiceAccountTokenNoAutoGeneration by default

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -987,7 +987,7 @@ var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureS
 	HonorPVReclaimPolicy:                           {Default: false, PreRelease: featuregate.Alpha},
 	RecoverVolumeExpansionFailure:                  {Default: false, PreRelease: featuregate.Alpha},
 	GRPCContainerProbe:                             {Default: false, PreRelease: featuregate.Alpha},
-	LegacyServiceAccountTokenNoAutoGeneration:      {Default: true, PreRelease: featuregate.Beta},
+	LegacyServiceAccountTokenNoAutoGeneration:      {Default: false, PreRelease: featuregate.Beta},
 
 	// inherited features from generic apiserver, relisted here to get a conflict if it is changed
 	// unintentionally on either side:
