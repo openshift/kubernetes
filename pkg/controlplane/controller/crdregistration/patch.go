8c807814e2e2cc4ebe9bfb2023ffa7218cade9fe
Alexander Demichev <demichev.alexander@gmail.com>
2022-05-11 14:39:59 +0200

    UPSTREAM: <carry>: kube-apiserver: prioritize machine api group

--- pkg/controlplane/controller/crdregistration/patch.go
+++ pkg/controlplane/controller/crdregistration/patch.go
@@ -2,6 +2,8 @@ package crdregistration
 
 func getGroupPriorityMin(group string) int32 {
 	switch group {
+	case "machine.openshift.io":
+		return 1100
 	case "config.openshift.io":
 		return 1100
 	case "operator.openshift.io":
