5a6b52e9b6144bb277fec449e8079c507d7cde15
Aravindh Puthiyaparambil <aravindh@redhat.com>
2023-04-12 10:46:15 -0700

    UPSTREAM: <carry>: Extend NodeLogQuery feature
    Extend the NodeLogQuery feature to support oc adm node-logs options:
    - Default NodeLogQuery feature gate to true
    - Add support for --since, --until, --case-sensitive, --output, options
    
    UPSTREAM: <carry>: Extend NodeLogQuery feature
    
    Fix handling of the "until" parameter when generating the journalctl
    command. This was incorrectly being passed with the "since" value.

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1131,7 +1131,7 @@ var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureS
 
 	NFTablesProxyMode: {Default: true, PreRelease: featuregate.Beta},
 
-	NodeLogQuery: {Default: false, PreRelease: featuregate.Beta},
+	NodeLogQuery: {Default: true, PreRelease: featuregate.Beta},
 
 	NodeOutOfServiceVolumeDetach: {Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // remove in 1.31
 
