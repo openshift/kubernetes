c7af7200ef787089bbf5255243aafdcba3c5f008
Abu Kashem <akashem@redhat.com>
2023-04-24 17:18:50 -0400

    UPSTREAM: <carry>: add conditional shutdown response header

--- staging/src/k8s.io/apiserver/pkg/server/filters/with_early_late_annotations.go
+++ staging/src/k8s.io/apiserver/pkg/server/filters/with_early_late_annotations.go
@@ -117,7 +117,6 @@ func withShutdownResponseHeader(handler http.Handler, shutdownInitiated lifecycl
 
 		w.Header().Set("X-OpenShift-Disruption", msgFn(true, clock.Since(*shutdownInitiatedAt)))
 		handler.ServeHTTP(w, req)
-		w.Header().Set("X-OpenShift-Shutdown", message)
 	})
 }
 
