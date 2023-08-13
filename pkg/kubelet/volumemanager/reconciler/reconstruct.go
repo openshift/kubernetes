5e84af6c3a0a135fde020641567d0e6e2ef78a6e
cvvz <ftdchenwz@gmail.com>
2023-08-13 15:52:25 +0800

    fix: 119921

--- pkg/kubelet/volumemanager/reconciler/reconstruct.go
+++ pkg/kubelet/volumemanager/reconciler/reconstruct.go
@@ -95,6 +95,7 @@ func (rc *reconciler) syncStates(kubeletPodDir string) {
 			klog.V(4).InfoS("Volume exists in desired state, marking as InUse", "podName", volume.podName, "volumeSpecName", volume.volumeSpecName)
 			continue
 		}
+		gvl.addPodVolume(reconstructedVolume)
 		// There is no pod that uses the volume.
 		if rc.operationExecutor.IsOperationPending(reconstructedVolume.volumeName, nestedpendingoperations.EmptyUniquePodName, nestedpendingoperations.EmptyNodeName) {
 			klog.InfoS("Volume is in pending operation, skip cleaning up mounts")
