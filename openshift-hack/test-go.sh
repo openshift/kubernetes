d4a0420dd5aab7d9ae411cf6b4f4394344d96c43
Maciej Szulik <maszulik@redhat.com>
2023-08-22 13:11:12 +0200

    UPSTREAM: <drop>: try to catch core dump

--- openshift-hack/test-go.sh
+++ openshift-hack/test-go.sh
@@ -3,8 +3,21 @@
 # shellcheck source=openshift-hack/lib/init.sh
 source "$(dirname "${BASH_SOURCE[0]}")/lib/init.sh"
 
+ulimit -c unlimited
+
 ARTIFACTS="${ARTIFACTS:-/tmp/artifacts}"
 mkdir -p "${ARTIFACTS}"
+
+save_core() {
+  echo "Checking for core files in $(pwd)..."
+  for i in $(find ./ -type f -iname core); do
+    if [ -f "$i" ] ; then
+        echo "Found core file $i..."
+        cp "$i" "${ARTIFACTS}"
+    fi
+  done
+}
+trap "save_core" EXIT SIGINT
 
 export KUBERNETES_SERVICE_HOST=
 export KUBE_JUNIT_REPORT_DIR="${ARTIFACTS}"
