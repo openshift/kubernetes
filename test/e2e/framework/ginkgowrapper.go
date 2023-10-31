aac4ce7dc9a266c15f2873a6a53ef9d2f8b98cef
Maciej Szulik <maszulik@redhat.com>
2023-10-31 16:34:56 +0100

    UPSTREAM: <carry>: use go1.20

--- test/e2e/framework/ginkgowrapper.go
+++ test/e2e/framework/ginkgowrapper.go
@@ -21,7 +21,6 @@ import (
 	"path"
 	"reflect"
 	"regexp"
-	"slices"
 	"strings"
 
 	"github.com/onsi/ginkgo/v2"
@@ -475,5 +474,17 @@ func TagsEqual(a, b interface{}) bool {
 	if al.extra != bl.extra {
 		return false
 	}
-	return slices.Equal(al.parts, bl.parts)
+	return slicesEqual(al.parts, bl.parts)
+}
+
+func slicesEqual[S ~[]E, E comparable](s1, s2 S) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i := range s1 {
+		if s1[i] != s2[i] {
+			return false
+		}
+	}
+	return true
 }
