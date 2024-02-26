b1f708760d4ebc2404ba9bad97d1c11ae2c6f9b7
Patrick Ohly <patrick.ohly@intel.com>
2024-02-26 12:53:04 +0100

    dra kubelet: publish NodeResourceSlices
    The information is received from the DRA driver plugin through a new gRPC
    streaming interface. This is backwards compatible with old DRA driver kubelet
    plugins, their gRPC server will return "not implemented" and that can be
    handled by kubelet. Therefore no API break is needed.
    
    However, DRA drivers need to be updated because the Go API changed. They can
    return
        status.New(codes.Unimplemented, "no node resource support").Err()
    if they don't support the new ListAndWatchResources method and
    structured parameters.
    
    The controller in kubelet then synchronizes this information from the driver
    with NodeResourceSlice objects, creating, updating and deleting them as needed.

--- pkg/kubelet/cm/container_manager_linux.go
+++ pkg/kubelet/cm/container_manager_linux.go
@@ -311,7 +311,7 @@ func NewContainerManager(mountUtil mount.Interface, cadvisorInterface cadvisor.I
 	// initialize DRA manager
 	if utilfeature.DefaultFeatureGate.Enabled(kubefeatures.DynamicResourceAllocation) {
 		klog.InfoS("Creating Dynamic Resource Allocation (DRA) manager")
-		cm.draManager, err = dra.NewManagerImpl(kubeClient, nodeConfig.KubeletRootDir)
+		cm.draManager, err = dra.NewManagerImpl(kubeClient, nodeConfig.KubeletRootDir, nodeConfig.NodeName)
 		if err != nil {
 			return nil, err
 		}
