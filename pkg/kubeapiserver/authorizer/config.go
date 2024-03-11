8423c3af8fcc0e61787139e78581482874b21555
Maciej Szulik <maszulik@redhat.com>
2024-03-11 21:43:17 +0100

    UPSTREAM: <carry>: openshift-kube-apiserver: add kube-apiserver patches - fixes

--- pkg/kubeapiserver/authorizer/config.go
+++ pkg/kubeapiserver/authorizer/config.go
@@ -23,8 +23,9 @@ import (
 	"strings"
 	"time"
 
-	"k8s.io/kubernetes/openshift-kube-apiserver/authorization/browsersafe"
 	"k8s.io/kubernetes/openshift-kube-apiserver/authorization/scopeauthorizer"
+
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 
 	utilnet "k8s.io/apimachinery/pkg/util/net"
 	"k8s.io/apimachinery/pkg/util/sets"
@@ -86,11 +87,7 @@ func (config Config) New(ctx context.Context, serverID string) (authorizer.Autho
 		reloadInterval:   time.Minute,
 	}
 
-	if !skipSystemMastersAuthorizer {
-		// Add SystemPrivilegedGroup as an authorizing group
-		superuserAuthorizer := authorizerfactory.NewPrivilegedGroups(user.SystemPrivilegedGroup)
-		authorizers = append(authorizers, superuserAuthorizer)
-	}
+	seenTypes := sets.New[authzconfig.AuthorizerType]()
 
 	// Build and store authorizers which will persist across reloads
 	for _, configuredAuthorizer := range config.AuthorizationConfiguration.Authorizers {
@@ -128,16 +125,9 @@ func (config Config) New(ctx context.Context, serverID string) (authorizer.Autho
 				&rbac.ClusterRoleGetter{Lister: config.VersionedInformerFactory.Rbac().V1().ClusterRoles().Lister()},
 				&rbac.ClusterRoleBindingLister{Lister: config.VersionedInformerFactory.Rbac().V1().ClusterRoleBindings().Lister()},
 			)
-			// Wrap with an authorizer that detects unsafe requests and modifies verbs/resources appropriately so policy can address them separately
-			authorizers = append(authorizers, browsersafe.NewBrowserSafeAuthorizer(rbacAuthorizer, user.AllAuthenticated))
-			ruleResolvers = append(ruleResolvers, rbacAuthorizer)
 		case authzconfig.AuthorizerType(modes.ModeScope):
 			// Wrap with an authorizer that detects unsafe requests and modifies verbs/resources appropriately so policy can address them separately
-			scopeLimitedAuthorizer := scopeauthorizer.NewAuthorizer(config.VersionedInformerFactory.Rbac().V1().ClusterRoles().Lister())
-			authorizers = append(authorizers, browsersafe.NewBrowserSafeAuthorizer(scopeLimitedAuthorizer, user.AllAuthenticated))
-		case authzconfig.AuthorizerType(modes.ModeSystemMasters):
-			// no browsersafeauthorizer here becase that rewrites the resources.  This authorizer matches no matter which resource matches.
-			authorizers = append(authorizers, authorizerfactory.NewPrivilegedGroups(user.SystemPrivilegedGroup))
+			r.scopeLimitedAuthorizer = scopeauthorizer.NewAuthorizer(config.VersionedInformerFactory.Rbac().V1().ClusterRoles().Lister())
 		default:
 			return nil, nil, fmt.Errorf("unknown authorization mode %s specified", configuredAuthorizer.Type)
 		}
