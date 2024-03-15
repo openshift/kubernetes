06c9e7799b9a0208e046e7eed19c5f2b789d9b9d
Swarup Ghosh <swghosh@redhat.com>
2024-03-15 18:41:09 +0530

    UPSTREAM: <carry>: Add openshift feature gates to kube-apiserver - inject openshift feature gates into pkg/features
    Signed-off-by: Swarup Ghosh <swghosh@redhat.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -991,6 +991,7 @@ const (
 )
 
 func init() {
+	registerOpenshiftFeatures()
 	runtime.Must(utilfeature.DefaultMutableFeatureGate.Add(defaultKubernetesFeatureGates))
 	runtime.Must(utilfeature.DefaultMutableFeatureGate.AddVersioned(defaultVersionedKubernetesFeatureGates))
 
