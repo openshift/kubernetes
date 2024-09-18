3660ec7a6bf177315da3959feb4dad0b97bb88c4
Fabio Bertinatto <fbertina@redhat.com>
2024-09-18 10:12:40 -0300

    UPSTREAM: <drop>: disable ResilientWatchCacheInitialization feature

--- staging/src/k8s.io/apiserver/pkg/features/kube_features.go
+++ staging/src/k8s.io/apiserver/pkg/features/kube_features.go
@@ -360,7 +360,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	},
 
 	ResilientWatchCacheInitialization: {
-		{Version: version.MustParse("1.31"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.31"), Default: false, PreRelease: featuregate.Beta},
 	},
 
 	RetryGenerateName: {
