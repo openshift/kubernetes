19b0da764a0e93826f668d6c3f531a40e1e7a4cb
everettraven <everettraven@gmail.com>
2024-12-10 16:32:53 -0500

    go fmt
    Signed-off-by: everettraven <everettraven@gmail.com>

--- openshift-kube-apiserver/admission/authorization/restrictusers/intializers.go
+++ openshift-kube-apiserver/admission/authorization/restrictusers/intializers.go
@@ -3,12 +3,12 @@ package restrictusers
 import (
 	"k8s.io/apiserver/pkg/admission"
 
-	userinformer "github.com/openshift/client-go/user/informers/externalversions"
 	configv1informer "github.com/openshift/client-go/config/informers/externalversions"
+	userinformer "github.com/openshift/client-go/user/informers/externalversions"
 )
 
 func NewInitializer(userInformer userinformer.SharedInformerFactory, configInformer configv1informer.SharedInformerFactory) admission.PluginInitializer {
-    return &localInitializer{userInformer: userInformer, configInformer: configInformer}
+	return &localInitializer{userInformer: userInformer, configInformer: configInformer}
 }
 
 type WantsUserInformer interface {
@@ -17,12 +17,12 @@ type WantsUserInformer interface {
 }
 
 type WantsConfigInformer interface {
-    SetConfigInformer(configv1informer.SharedInformerFactory)
+	SetConfigInformer(configv1informer.SharedInformerFactory)
 }
 
 type localInitializer struct {
-	userInformer userinformer.SharedInformerFactory
-    configInformer configv1informer.SharedInformerFactory
+	userInformer   userinformer.SharedInformerFactory
+	configInformer configv1informer.SharedInformerFactory
 }
 
 // Initialize will check the initialization interfaces implemented by each plugin
@@ -32,7 +32,7 @@ func (i *localInitializer) Initialize(plugin admission.Interface) {
 		wants.SetUserInformer(i.userInformer)
 	}
 
-    if wants, ok := plugin.(WantsConfigInformer); ok {
-        wants.SetConfigInformer(i.configInformer)
-    }
+	if wants, ok := plugin.(WantsConfigInformer); ok {
+		wants.SetConfigInformer(i.configInformer)
+	}
 }
