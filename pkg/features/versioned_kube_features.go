f346c26c0c95b57285798fce657144a8d2621676
Marek Siarkowicz <siarkowicz@google.com>
2024-12-19 12:30:39 +0100

    UPSTREAM: 129407: Implement streaming proto encoding

--- pkg/features/versioned_kube_features.go
+++ pkg/features/versioned_kube_features.go
@@ -35,4 +35,8 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	StreamingCollectionEncodingToJSON: {
 		{Version: version.MustParse("1.33"), Default: true, PreRelease: featuregate.Beta},
 	},
+
+	StreamingCollectionEncodingToProtobuf: {
+		{Version: version.MustParse("1.33"), Default: true, PreRelease: featuregate.Beta},
+	},
 }
