f3e36afb640fae1c7b52a53ffb13864731ab6001
Jeremy Peterson <jerpeter@redhat.com>
2025-02-18 18:32:57 +0000

    UPSTREAM: <drop>: manually resolve conflicts

--- hack/verify-govulncheck.sh
+++ hack/verify-govulncheck.sh
@@ -27,11 +27,7 @@ kube::util::ensure_clean_working_dir
 # This sets up the environment, like GOCACHE, which keeps the worktree cleaner.
 kube::golang::setup_env
 
-<<<<<<< HEAD
-go install -mod=readonly golang.org/x/vuln/cmd/govulncheck@v1.0.1
-=======
-go install golang.org/x/vuln/cmd/govulncheck@v1.1.2
->>>>>>> v1.30.10
+go install -mod=readonly golang.org/x/vuln/cmd/govulncheck@v1.1.2
 
 # KUBE_VERIFY_GIT_BRANCH is populated in verify CI jobs
 BRANCH="${KUBE_VERIFY_GIT_BRANCH:-master}"
