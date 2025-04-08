cc4766160ab5db2e0f1333ff915f7a9431930e1b
Dan Winship <danwinship@redhat.com>
2025-04-08 11:14:44 -0400

    DNM make e2e.test always pass

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -43,27 +43,6 @@ import (
 	// define and freeze constants
 	_ "k8s.io/kubernetes/test/e2e/feature"
 
-	// test sources
-	_ "k8s.io/kubernetes/test/e2e/apimachinery"
-	_ "k8s.io/kubernetes/test/e2e/apps"
-	_ "k8s.io/kubernetes/test/e2e/architecture"
-	_ "k8s.io/kubernetes/test/e2e/auth"
-	_ "k8s.io/kubernetes/test/e2e/autoscaling"
-	_ "k8s.io/kubernetes/test/e2e/cloud"
-	_ "k8s.io/kubernetes/test/e2e/common"
-	_ "k8s.io/kubernetes/test/e2e/dra"
-	_ "k8s.io/kubernetes/test/e2e/instrumentation"
-	_ "k8s.io/kubernetes/test/e2e/kubectl"
-	_ "k8s.io/kubernetes/test/e2e/lifecycle"
-	_ "k8s.io/kubernetes/test/e2e/lifecycle/bootstrap"
-	_ "k8s.io/kubernetes/test/e2e/network"
-	_ "k8s.io/kubernetes/test/e2e/node"
-	_ "k8s.io/kubernetes/test/e2e/scheduling"
-	_ "k8s.io/kubernetes/test/e2e/storage"
-	_ "k8s.io/kubernetes/test/e2e/storage/csimock"
-	_ "k8s.io/kubernetes/test/e2e/storage/external"
-	_ "k8s.io/kubernetes/test/e2e/windows"
-
 	// reconfigure framework
 	_ "k8s.io/kubernetes/test/e2e/framework/debug/init"
 	_ "k8s.io/kubernetes/test/e2e/framework/metrics/init"
@@ -79,10 +58,8 @@ func TestMain(m *testing.M) {
 	// Register test flags, then parse flags.
 	HandleFlags()
 
-	if versionFlag {
 		fmt.Printf("%s\n", version.Get())
 		os.Exit(0)
-	}
 
 	if flag.CommandLine.NArg() > 0 {
 		fmt.Fprintf(os.Stderr, "unknown additional command line arguments: %s", flag.CommandLine.Args())
@@ -134,7 +111,7 @@ func TestMain(m *testing.M) {
 }
 
 func TestE2E(t *testing.T) {
-	RunE2ETests(t)
+		os.Exit(0)
 }
 
 var _ = ginkgo.ReportAfterEach(func(report ginkgo.SpecReport) {
