c34aa829d97179b6990346ae67b7b45081f68bd7
Neeraj Krishna Gopalakrishna <ngopalak@redhat.com>
2025-06-18 19:46:12 +0530

    UPSTREAM: <carry>: OCPBUGS-57477: Make e2e test to fetch metrics/resource serial

--- openshift-hack/cmd/k8s-tests-ext/labels.go
+++ openshift-hack/cmd/k8s-tests-ext/labels.go
@@ -37,6 +37,8 @@ func addLabelsToSpecs(specs et.ExtensionTestSpecs) {
 			"should prevent Ingress creation if more than 1 IngressClass marked as default", // https://bugzilla.redhat.com/show_bug.cgi?id=1822286
 
 			"[sig-network] IngressClass [Feature:Ingress] should set default value on new IngressClass", //https://bugzilla.redhat.com/show_bug.cgi?id=1833583
+
+			"[sig-instrumentation] Metrics should grab all metrics from kubelet /metrics/resource endpoint", // Flaky test - Connectivity issue using proxy
 		},
 	}
 
