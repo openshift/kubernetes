6e40b29c4f1786da9357afd5f03f6a4a6f80e596
Bryce Palmer <bpalmer@redhat.com>
2025-07-01 13:38:47 -0400

    UPSTREAM: <carry>: admission: authentication: validate CEL expressions compilation
    Signed-off-by: Bryce Palmer <bpalmer@redhat.com>

--- openshift-kube-apiserver/admission/customresourcevalidation/authentication/validate_authentication.go
+++ openshift-kube-apiserver/admission/customresourcevalidation/authentication/validate_authentication.go
@@ -4,14 +4,20 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"time"
 
+	"golang.org/x/sync/singleflight"
 	"k8s.io/apimachinery/pkg/api/validation"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/apiserver/pkg/admission"
+	"k8s.io/apiserver/pkg/warning"
+	"k8s.io/klog/v2"
+	"k8s.io/utils/lru"
 
 	configv1 "github.com/openshift/api/config/v1"
+	authenticationcel "k8s.io/apiserver/pkg/authentication/cel"
 	crvalidation "k8s.io/kubernetes/openshift-kube-apiserver/admission/customresourcevalidation"
 )
 
@@ -25,7 +31,13 @@ func Register(plugins *admission.Plugins) {
 				configv1.GroupVersion.WithResource("authentications").GroupResource(): true,
 			},
 			map[schema.GroupVersionKind]crvalidation.ObjectValidator{
-				configv1.GroupVersion.WithKind("Authentication"): authenticationV1{},
+				configv1.GroupVersion.WithKind("Authentication"): authenticationV1{
+					cel: &celStore{
+						compiledStore:  lru.New(100),
+						compilingGroup: new(singleflight.Group),
+						compiler:       authenticationcel.NewDefaultCompiler(),
+					},
+				},
 			})
 	})
 }
@@ -46,21 +58,34 @@ func toAuthenticationV1(uncastObj runtime.Object) (*configv1.Authentication, fie
 	return obj, nil
 }
 
-type authenticationV1 struct{}
+type celStore struct {
+	compilingGroup *singleflight.Group
+	compiledStore  *lru.Cache
+	compiler       authenticationcel.Compiler
+}
 
-func (authenticationV1) ValidateCreate(_ context.Context, uncastObj runtime.Object) field.ErrorList {
+type authenticationV1 struct {
+	cel *celStore
+}
+
+func (a authenticationV1) ValidateCreate(ctx context.Context, uncastObj runtime.Object) field.ErrorList {
 	obj, errs := toAuthenticationV1(uncastObj)
 	if len(errs) > 0 {
 		return errs
 	}
 
 	errs = append(errs, validation.ValidateObjectMeta(&obj.ObjectMeta, false, crvalidation.RequireNameCluster, field.NewPath("metadata"))...)
-	errs = append(errs, validateAuthenticationSpecCreate(obj.Spec)...)
+	err, warns := validateAuthenticationSpecCreate(ctx, obj.Spec, a.cel)
+	errs = append(errs, err...)
+	for _, warn := range warns {
+		warning.AddWarning(ctx, "", warn)
+		klog.Warning(warn)
+	}
 
 	return errs
 }
 
-func (authenticationV1) ValidateUpdate(_ context.Context, uncastObj runtime.Object, uncastOldObj runtime.Object) field.ErrorList {
+func (a authenticationV1) ValidateUpdate(ctx context.Context, uncastObj runtime.Object, uncastOldObj runtime.Object) field.ErrorList {
 	obj, errs := toAuthenticationV1(uncastObj)
 	if len(errs) > 0 {
 		return errs
@@ -71,7 +96,12 @@ func (authenticationV1) ValidateUpdate(_ context.Context, uncastObj runtime.Obje
 	}
 
 	errs = append(errs, validation.ValidateObjectMetaUpdate(&obj.ObjectMeta, &oldObj.ObjectMeta, field.NewPath("metadata"))...)
-	errs = append(errs, validateAuthenticationSpecUpdate(obj.Spec, oldObj.Spec)...)
+
+	err, warns := validateAuthenticationSpecUpdate(ctx, obj.Spec, oldObj.Spec, a.cel)
+	errs = append(errs, err...)
+	for _, warn := range warns {
+		warning.AddWarning(ctx, "", warn)
+	}
 
 	return errs
 }
@@ -92,16 +122,17 @@ func (authenticationV1) ValidateStatusUpdate(_ context.Context, uncastObj runtim
 	return errs
 }
 
-func validateAuthenticationSpecCreate(spec configv1.AuthenticationSpec) field.ErrorList {
-	return validateAuthenticationSpec(spec)
+func validateAuthenticationSpecCreate(ctx context.Context, spec configv1.AuthenticationSpec, cel *celStore) (field.ErrorList, []string) {
+	return validateAuthenticationSpec(ctx, spec, cel)
 }
 
-func validateAuthenticationSpecUpdate(newspec, oldspec configv1.AuthenticationSpec) field.ErrorList {
-	return validateAuthenticationSpec(newspec)
+func validateAuthenticationSpecUpdate(ctx context.Context, newspec, oldspec configv1.AuthenticationSpec, cel *celStore) (field.ErrorList, []string) {
+	return validateAuthenticationSpec(ctx, newspec, cel)
 }
 
-func validateAuthenticationSpec(spec configv1.AuthenticationSpec) field.ErrorList {
+func validateAuthenticationSpec(ctx context.Context, spec configv1.AuthenticationSpec, cel *celStore) (field.ErrorList, []string) {
 	errs := field.ErrorList{}
+	warns := []string{}
 	specField := field.NewPath("spec")
 
 	if spec.WebhookTokenAuthenticator != nil {
@@ -121,14 +152,181 @@ func validateAuthenticationSpec(spec configv1.AuthenticationSpec) field.ErrorLis
 				spec.WebhookTokenAuthenticator, fmt.Sprintf("this field cannot be set with the %q .spec.type", spec.Type),
 			))
 		}
-
 	}
 
 	errs = append(errs, crvalidation.ValidateConfigMapReference(specField.Child("oauthMetadata"), spec.OAuthMetadata, false)...)
 
-	return errs
+	// Perform External OIDC Provider related validations
+	// ----------------
+
+	// There is currently no guarantee that these fields are not set when the spec.Type is != OIDC.
+	// To ensure we are enforcing approriate admission validations at all times, just always iterate through the list
+	// of OIDC Providers and perform the validations.
+	// If/when the openshift/api admission validations are updated to enforce that this field is not configured
+	// when Type != OIDC, this loop should be a no-op due to an empty list.
+	for i, provider := range spec.OIDCProviders {
+		err, warnings := validateOIDCProvider(ctx, specField.Child("oidcProviders").Index(i), cel, provider)
+		errs = append(errs, err...)
+		warns = append(warns, warnings...)
+	}
+	// ----------------
+
+	return errs, warns
 }
 
 func validateAuthenticationStatus(status configv1.AuthenticationStatus) field.ErrorList {
 	return crvalidation.ValidateConfigMapReference(field.NewPath("status", "integratedOAuthMetadata"), status.IntegratedOAuthMetadata, false)
+}
+
+func validateOIDCProvider(ctx context.Context, path *field.Path, cel *celStore, provider configv1.OIDCProvider) (field.ErrorList, []string) {
+	return validateClaimMappings(ctx, path, cel, provider.ClaimMappings)
+}
+
+func validateClaimMappings(ctx context.Context, path *field.Path, cel *celStore, claimMappings configv1.TokenClaimMappings) (field.ErrorList, []string) {
+	path = path.Child("claimMappings")
+	errs := field.ErrorList{}
+	warns := []string{}
+
+	err, warn := validateUIDClaimMapping(ctx, path, cel, claimMappings.UID)
+	errs = append(errs, err...)
+	if len(warn) > 0 {
+		warns = append(warns, warn)
+	}
+
+	err, warnings := validateExtraClaimMapping(ctx, path, cel, claimMappings.Extra...)
+	errs = append(errs, err...)
+	warns = append(warns, warnings...)
+
+	return errs, warns
+}
+
+func validateUIDClaimMapping(ctx context.Context, path *field.Path, cel *celStore, uid *configv1.TokenClaimOrExpressionMapping) (field.ErrorList, string) {
+	if uid == nil {
+		return nil, ""
+	}
+
+	var warn string
+	if uid.Expression != "" {
+		childPath := path.Child("uid", "expression")
+		var err error
+		err, warn = validateCELExpression(ctx, cel, &authenticationcel.ClaimMappingExpression{
+			Expression: uid.Expression,
+		})
+
+		if len(warn) > 0 {
+			warn = fmt.Sprintf("validating %s: %s", childPath, warn)
+		}
+
+		if err != nil {
+			return field.ErrorList{field.Invalid(childPath, uid.Expression, err.Error())}, warn
+		}
+	}
+
+	return nil, warn
+}
+
+func validateExtraClaimMapping(ctx context.Context, path *field.Path, cel *celStore, extras ...configv1.ExtraMapping) (field.ErrorList, []string) {
+	errs := field.ErrorList{}
+	warns := []string{}
+	for i, extra := range extras {
+		err, warn := validateExtra(ctx, path.Child("extra").Index(i), cel, extra)
+		errs = append(errs, err...)
+		if len(warn) > 0 {
+			warns = append(warns, warn)
+		}
+	}
+	return errs, warns
+}
+
+func validateExtra(ctx context.Context, path *field.Path, cel *celStore, extra configv1.ExtraMapping) (field.ErrorList, string) {
+	childPath := path.Child("valueExpression")
+	err, warn := validateCELExpression(ctx, cel, &authenticationcel.ExtraMappingExpression{
+		Key:        extra.Key,
+		Expression: extra.ValueExpression,
+	})
+
+	if len(warn) > 0 {
+		warn = fmt.Sprintf("validating %s: %s", childPath, warn)
+	}
+
+	if err != nil {
+		return field.ErrorList{field.Invalid(childPath, extra.ValueExpression, err.Error())}, warn
+	}
+
+	return nil, warn
+}
+
+type compileResult struct {
+	err  error
+	warn string
+}
+
+func validateCELExpression(ctx context.Context, cel *celStore, accessor authenticationcel.ExpressionAccessor) (error, string) {
+	// if context has been canceled, don't try to compile any expressions
+	if err := ctx.Err(); err != nil {
+		return err, ""
+	}
+
+	result, err, _ := cel.compilingGroup.Do(accessor.GetExpression(), func() (interface{}, error) {
+		// if the expression is not currently being compiled, it might have already been compiled
+		if val, ok := cel.compiledStore.Get(accessor.GetExpression()); ok {
+			if val != nil {
+				res := val.(compileResult)
+				return res, nil
+			}
+
+			return nil, nil
+		}
+
+		// expression is not currently being compiled, and has not been compiled before (or has been long enough since it was last compiled that we dropped it).
+		// Let's compile it.
+		warningString := ""
+		compiled := make(chan error)
+		defer close(compiled)
+
+		go func() {
+			defer func() {
+				if r := recover(); r != nil {
+					// convert the panic into an error state for the expression
+					compiled <- fmt.Errorf("recovered from a panic while compiling expression %q: %v", accessor.GetExpression(), r)
+				}
+			}()
+
+			_, err := cel.compiler.CompileClaimsExpression(accessor)
+
+			compiled <- err
+		}()
+
+		warning := make(chan string, 1)
+		timer := time.AfterFunc(time.Second, func() {
+			defer close(warning)
+			warning <- fmt.Sprintf("cel expression %q took more than 1 second to compile", accessor.GetExpression())
+		})
+
+		err := <-compiled
+
+		timer.Stop()
+
+		// check if we received a warning. If not, continue
+		select {
+		case warn := <-warning:
+			warningString = warn
+		default:
+			break
+		}
+
+		compilationResult := compileResult{
+			err, warningString,
+		}
+
+		cel.compiledStore.Add(accessor.GetExpression(), compilationResult)
+
+		return compilationResult, nil
+	})
+	if err != nil {
+		return fmt.Errorf("running compilation of expression %q: %v", accessor.GetExpression(), err), ""
+	}
+
+	compileRes := result.(compileResult)
+	return compileRes.err, compileRes.warn
 }
--- openshift-kube-apiserver/admission/customresourcevalidation/authentication/validate_authentication_test.go
+++ openshift-kube-apiserver/admission/customresourcevalidation/authentication/validate_authentication_test.go
@@ -1,10 +1,16 @@
 package authentication
 
 import (
+	"context"
+	"errors"
 	"testing"
+	"time"
 
 	configv1 "github.com/openshift/api/config/v1"
+	"golang.org/x/sync/singleflight"
 	"k8s.io/apimachinery/pkg/util/validation/field"
+	authenticationcel "k8s.io/apiserver/pkg/authentication/cel"
+	"k8s.io/utils/lru"
 )
 
 func TestFailValidateAuthenticationSpec(t *testing.T) {
@@ -49,10 +55,49 @@ func TestFailValidateAuthenticationSpec(t *testing.T) {
 			errorType:  field.ErrorTypeInvalid,
 			errorField: "spec.webhookTokenAuthenticator",
 		},
+		"invalid UID CEL expression": {
+			spec: configv1.AuthenticationSpec{
+				Type: "OIDC",
+				OIDCProviders: []configv1.OIDCProvider{
+					{
+						ClaimMappings: configv1.TokenClaimMappings{
+							UID: &configv1.TokenClaimOrExpressionMapping{
+								Expression: "!@^#&(!^@(*#&(",
+							},
+						},
+					},
+				},
+			},
+			errorType:  field.ErrorTypeInvalid,
+			errorField: "spec.oidcProviders[0].claimMappings.uid.expression",
+		},
+		"invalid Extra CEL expression": {
+			spec: configv1.AuthenticationSpec{
+				Type: "OIDC",
+				OIDCProviders: []configv1.OIDCProvider{
+					{
+						ClaimMappings: configv1.TokenClaimMappings{
+							Extra: []configv1.ExtraMapping{
+								{
+									Key:             "foo/bar",
+									ValueExpression: "!@*(&#^(!@*)&^&",
+								},
+							},
+						},
+					},
+				},
+			},
+			errorType:  field.ErrorTypeInvalid,
+			errorField: "spec.oidcProviders[0].claimMappings.extra[0].valueExpression",
+		},
 	}
 
 	for tcName, tc := range errorCases {
-		errs := validateAuthenticationSpec(tc.spec)
+		errs, _ := validateAuthenticationSpec(context.TODO(), tc.spec, &celStore{
+			compiler:       authenticationcel.NewDefaultCompiler(),
+			compilingGroup: new(singleflight.Group),
+			compiledStore:  lru.New(100),
+		})
 		if (len(errs) > 0) != (len(tc.errorType) != 0) {
 			t.Errorf("'%s': expected failure: %t, got: %t", tcName, len(tc.errorType) != 0, len(errs) > 0)
 		}
@@ -109,10 +154,41 @@ func TestSucceedValidateAuthenticationSpec(t *testing.T) {
 				{KubeConfig: configv1.SecretNameReference{Name: "thisisawebhook33"}},
 			},
 		},
+		"valid uid CEL expression": {
+			Type: "OIDC",
+			OIDCProviders: []configv1.OIDCProvider{
+				{
+					ClaimMappings: configv1.TokenClaimMappings{
+						UID: &configv1.TokenClaimOrExpressionMapping{
+							Expression: "claims.uid",
+						},
+					},
+				},
+			},
+		},
+		"valid Extra CEL expression": {
+			Type: "OIDC",
+			OIDCProviders: []configv1.OIDCProvider{
+				{
+					ClaimMappings: configv1.TokenClaimMappings{
+						Extra: []configv1.ExtraMapping{
+							{
+								Key:             "foo/bar",
+								ValueExpression: "claims.roles",
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 
 	for tcName, s := range successCases {
-		errs := validateAuthenticationSpec(s)
+		errs, _ := validateAuthenticationSpec(context.TODO(), s, &celStore{
+			compiler:       authenticationcel.NewDefaultCompiler(),
+			compilingGroup: new(singleflight.Group),
+			compiledStore:  lru.New(100),
+		})
 		if len(errs) != 0 {
 			t.Errorf("'%s': expected success, but failed: %v", tcName, errs.ToAggregate().Error())
 		}
@@ -175,5 +251,171 @@ func TestSucceedValidateAuthenticationStatus(t *testing.T) {
 			t.Errorf("'%s': expected success, but failed: %v", tcName, errs.ToAggregate().Error())
 		}
 	}
+}
 
+func TestValidateCELExpression(t *testing.T) {
+	type testcase struct {
+		name       string
+		cel        func() *celStore
+		ctx        func() context.Context
+		shouldErr  bool
+		shouldWarn bool
+	}
+
+	expression := &authenticationcel.ClaimMappingExpression{
+		Expression: `["foo", "bar"].exists(x, x == "foo")`,
+	}
+
+	testcases := []testcase{
+		{
+			name: "does not return a warning when compilation takes less than a second",
+			cel: func() *celStore {
+				return &celStore{
+					compiler: &mockCompiler{
+						delay: 200 * time.Millisecond,
+						err:   nil,
+					},
+					compilingGroup: new(singleflight.Group),
+					compiledStore:  lru.New(1),
+				}
+			},
+			ctx: func() context.Context { return context.TODO() },
+		},
+		{
+			name: "returns a warning when compilation takes longer than a second",
+			cel: func() *celStore {
+				return &celStore{
+					compiler: &mockCompiler{
+						delay: 1500 * time.Millisecond,
+						err:   nil,
+					},
+					compilingGroup: new(singleflight.Group),
+					compiledStore:  lru.New(1),
+				}
+			},
+			ctx:        func() context.Context { return context.TODO() },
+			shouldWarn: true,
+		},
+		{
+			name: "still returns error if compilation takes longer than a second and errors out",
+			cel: func() *celStore {
+				return &celStore{
+					compiler: &mockCompiler{
+						delay: 1500 * time.Millisecond,
+						err:   errors.New("boom"),
+					},
+					compilingGroup: new(singleflight.Group),
+					compiledStore:  lru.New(1),
+				}
+			},
+			ctx:        func() context.Context { return context.TODO() },
+			shouldWarn: true,
+			shouldErr:  true,
+		},
+		{
+			name: "returns an error if the context has been canceled",
+			cel: func() *celStore {
+				return &celStore{
+					compiler: &mockCompiler{
+						delay: 1500 * time.Millisecond,
+						err:   nil,
+					},
+					compilingGroup: new(singleflight.Group),
+					compiledStore:  lru.New(1),
+				}
+			},
+			ctx: func() context.Context {
+				ctx, cancel := context.WithCancel(context.TODO())
+				cancel()
+				return ctx
+			},
+			shouldErr: true,
+		},
+		{
+			name: "waits for already compiling expression to finish compiling and returns its results",
+			cel: func() *celStore {
+				compGroup := new(singleflight.Group)
+
+				_ = compGroup.DoChan(expression.Expression, func() (interface{}, error) {
+					// Hog the group for a bit
+					time.Sleep(time.Second)
+
+					return compileResult{
+						err:  errors.New("boom"),
+						warn: "warning",
+					}, nil
+				})
+
+				return &celStore{
+					compiler:       nil, // should never end up calling this
+					compilingGroup: compGroup,
+					compiledStore:  lru.New(1),
+				}
+			},
+			ctx:        func() context.Context { return context.TODO() },
+			shouldErr:  true,
+			shouldWarn: true,
+		},
+		{
+			name: "returns already compiled expression results if the expression has been compiled before",
+			cel: func() *celStore {
+				compiledLRU := lru.New(1)
+				res := compileResult{
+					err:  errors.New("boom"),
+					warn: "warning",
+				}
+				compiledLRU.Add(expression.Expression, res)
+
+				return &celStore{
+					compiler:       nil, // should never end up calling this
+					compilingGroup: new(singleflight.Group),
+					compiledStore:  compiledLRU,
+				}
+			},
+			ctx:        func() context.Context { return context.TODO() },
+			shouldErr:  true,
+			shouldWarn: true,
+		},
+		{
+			name: "handles panic in compilation goroutine",
+			cel: func() *celStore {
+				return &celStore{
+					compiler:       nil, // causes panic
+					compilingGroup: new(singleflight.Group),
+					compiledStore:  lru.New(1),
+				}
+			},
+			ctx:       func() context.Context { return context.TODO() },
+			shouldErr: true,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			err, warn := validateCELExpression(tc.ctx(), tc.cel(), expression)
+
+			if tc.shouldErr != (err != nil) {
+				t.Fatalf("error expectation does not match actual. expected? %v . received: %v", tc.shouldErr, err)
+			}
+
+			if tc.shouldWarn != (len(warn) > 0) {
+				t.Fatalf("warning expectation does not match actual. expected? %v . received: %s", tc.shouldErr, warn)
+			}
+		})
+	}
+}
+
+type mockCompiler struct {
+	delay time.Duration
+	err   error
+}
+
+func (mc *mockCompiler) CompileClaimsExpression(expressionAccessor authenticationcel.ExpressionAccessor) (authenticationcel.CompilationResult, error) {
+	time.Sleep(mc.delay)
+	return authenticationcel.CompilationResult{}, mc.err
+}
+
+func (mc *mockCompiler) CompileUserExpression(expressionAccessor authenticationcel.ExpressionAccessor) (authenticationcel.CompilationResult, error) {
+	time.Sleep(mc.delay)
+	return authenticationcel.CompilationResult{}, mc.err
 }
