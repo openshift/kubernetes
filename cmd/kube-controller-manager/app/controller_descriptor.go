84398c42ee048eb8d1c4000c4d81d6fd89ba3a3d
Jacob See <5027680+jacobsee@users.noreply.github.com>
2025-09-24 16:11:20 -0700

    UPSTREAM: <carry>: Ensure service ca is mounted for projected tokens
    OpenShift since 3.x has injected the service serving certificate
    ca (service ca) bundle into service account token secrets. This was
    intended to ensure that all pods would be able to easily verify
    connections to endpoints secured with service serving
    certificates. Since breaking customer workloads is not an option, and
    there is no way to ensure that customers are not relying on the
    service ca bundle being mounted at
    /var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt, it is
    necessary to continue mounting the service ca bundle in the same
    location in the bound token projected volumes enabled by the
    BoundServiceAccountTokenVolume feature (enabled by default in 1.21).
    
    A new controller is added to create a configmap per namespace that is
    annotated for service ca injection. The controller is derived from the
    controller that creates configmaps for the root ca. The service
    account admission controller is updated to include a source for the
    new configmap in the default projected volume definition.
    
    UPSTREAM: <carry>: <squash> Add unit testing for service ca configmap publishing
    
    This commit should be squashed with:
    
    UPSTREAM: <carry>: Ensure service ca is mounted for projected tokens
    
    OpenShift-Rebase-Source: d69d054c1ce
    
    UPSTREAM: <carry>: Ensure service ca is mounted for projected tokens
    
    UPSTREAM: <carry>: Update service CA cert publisher controller & controller descriptor to new style
    
    There is a new style upstream for setting up controllers and controller descriptors in this file. Squash with `UPSTREAM: <carry>: Ensure service ca is mounted for projected tokens` when appropriate.

--- cmd/kube-controller-manager/app/controller_descriptor.go
+++ cmd/kube-controller-manager/app/controller_descriptor.go
@@ -224,6 +224,7 @@ func NewControllerDescriptors() map[string]*ControllerDescriptor {
 	register(newTTLAfterFinishedControllerDescriptor())
 	register(newRootCACertificatePublisherControllerDescriptor())
 	register(newKubeAPIServerSignerClusterTrustBundledPublisherDescriptor())
+	register(newServiceCACertPublisherControllerDescriptor())
 	register(newEphemeralVolumeControllerDescriptor())
 
 	// feature gated
