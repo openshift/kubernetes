bb0ab59fedb2cedc8232b649b8fbb20ad155728c
Ben Luddy <bluddy@redhat.com>
2025-10-06 17:40:01 -0400

    poc: avoid extra heap alloc per item in json list encoding special case

--- staging/src/k8s.io/apimachinery/pkg/runtime/serializer/json/collections.go
+++ staging/src/k8s.io/apimachinery/pkg/runtime/serializer/json/collections.go
@@ -215,11 +215,13 @@ func encodeKeyValuePair(w io.Writer, key string, value any, suffix []byte) (err 
 }
 
 func encodeValue(w io.Writer, value any, suffix []byte) error {
-	data, err := json.Marshal(value)
-	if err != nil {
-		return err
-	}
-	_, err = w.Write(data)
+	// Use an Encoder to avoid the extra []byte allocated by Marshal. Encode, unlike Marshal,
+	// appends a trailing newline to separate consecutive encodings of JSON values that aren't
+	// self-delimiting, like numbers. Strip the newline to avoid the assumption that every
+	// json.Unmarshaler implementation will accept trailing whitespace.
+	enc := json.NewEncoder(&trailingLinefeedSuppressor{delegate: w})
+	enc.SetIndent("", "")
+	err := enc.Encode(value)
 	if err != nil {
 		return err
 	}
@@ -227,4 +229,49 @@ func encodeValue(w io.Writer, value any, suffix []byte) error {
 		_, err = w.Write(suffix)
 	}
 	return err
+}
+
+// trailingLinefeedSuppressor is an io.Writer that wraps another io.Writer, suppressing a single
+// trailing linefeed if it is the last byte written by the latest call to Write.
+type trailingLinefeedSuppressor struct {
+	lf       bool
+	delegate io.Writer
+}
+
+func (w *trailingLinefeedSuppressor) Write(p []byte) (int, error) {
+	if len(p) == 0 {
+		// Avoid flushing a buffered linefeeds on an empty write.
+		return 0, nil
+	}
+
+	if w.lf {
+		// The previous write had a trailing linefeed that was buffered. That wasn't the
+		// last Write call, so flush the buffered linefeed before continuing.
+		n, err := w.delegate.Write([]byte{'\n'})
+		if n > 0 {
+			w.lf = false
+		}
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	if p[len(p)-1] != '\n' {
+		return w.delegate.Write(p)
+	}
+
+	p = p[:len(p)-1]
+
+	if len(p) == 0 { // []byte{'\n'}
+		w.lf = true
+		return 1, nil
+	}
+
+	n, err := w.delegate.Write(p)
+	if n == len(p) {
+		// Everything up to the trailing linefeed has been flushed. Eat the linefeed.
+		w.lf = true
+		n++
+	}
+	return n, err
 }
