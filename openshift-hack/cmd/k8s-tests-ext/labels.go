ce2ef96652d36edc0dbe713ddc2e4257af4e4744
Max Cao <macao@redhat.com>
2025-10-20 20:35:26 -0700

    UPSTREAM: <carry>: move disabled HPA tests from Broken to DedicatedJob
    This commit moves the disabled HPA tests reason from "Broken" to a new section called "DedicatedJob".
    This section selects tests that that will filtered out of OTE suites, and instead run in a dedicated prow job.
    
    They are also still skipped by default when running the openshift-hack/test-kubernetes-e2e.sh script.
    This is because these tests use a ton of resources and flake in a high concurrency environment.
    
    Instead, a new HPA-only prow job will be configured to run these tests, with a lower concurrency.
    It will use the test-kubernetes-e2e.sh script and override the ginkgo --focus argument.
    
    Signed-off-by: Max Cao <macao@redhat.com>

--- openshift-hack/cmd/k8s-tests-ext/labels.go
+++ openshift-hack/cmd/k8s-tests-ext/labels.go
@@ -17,8 +17,6 @@ func addLabelsToSpecs(specs et.ExtensionTestSpecs) {
 
 			"Should be able to support the 1.7 Sample API Server using the current Aggregator", // down apiservices break other clients today https://bugzilla.redhat.com/show_bug.cgi?id=1623195
 
-			"[Feature:HPA] Horizontal pod autoscaling (scale resource: CPU) [sig-autoscaling] ReplicationController light Should scale from 1 pod to 2 pods",
-
 			"should prevent Ingress creation if more than 1 IngressClass marked as default", // https://bugzilla.redhat.com/show_bug.cgi?id=1822286
 
 			"[sig-network] IngressClass [Feature:Ingress] should set default value on new IngressClass", //https://bugzilla.redhat.com/show_bug.cgi?id=1833583
