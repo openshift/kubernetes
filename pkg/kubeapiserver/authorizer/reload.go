f1e8bb8539dda9ba25f5a37f80bf02c492b8b032
Jacob See <5027680+jacobsee@users.noreply.github.com>
2026-02-06 17:15:09 -0800

    UPSTREAM: <carry>: admission: validate minimumKubeletVersion
    UPSTREAM: <carry>: admission: validate minimumKubeletVersion
    Signed-off-by: Peter Hunt <pehunt@redhat.com>
    
    UPSTREAM: <carry>: authorization: add minimumkubeletversion package
    
    MinimumKubeletVersion is a way for an admin to declare that nodes any older than the
    minimum version cannot authorize with the apiserver. This effectively prevents them from joining.
    
    Doing so means the apiservers can trust newer features are usable on clusters with version skews
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>
    
    UPSTREAM: <carry>: authorizer: move mininum kubelet version authorizer to pkg/kubeapiserver and add authorization mode
    
    this does require a line of code be moved from the enablement package to stop a cyclical import
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>
    
    UPSTREAM: <carry>: crdvalidation: move latency profile file to be agnostic of field
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>
    
    UPSTREAM: <carry>: features: add MinimumKubeletVersion feature
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>
    
    UPSTREAM: <carry>: Feature gates must now declare dependencies, even if there are none.
    
    UPSTREAM: <carry>: authorizer.Authorizer now requires ConditionsAwareAuthorize and EvaluateConditions and related changes
    
    Squash into admission: validate minimumKubeletVersion

--- pkg/kubeapiserver/authorizer/reload.go
+++ pkg/kubeapiserver/authorizer/reload.go
@@ -28,6 +28,7 @@ import (
 	"time"
 
 	"k8s.io/kubernetes/openshift-kube-apiserver/authorization/browsersafe"
+	"k8s.io/kubernetes/openshift-kube-apiserver/authorization/minimumkubeletversion"
 
 	"k8s.io/apimachinery/pkg/util/sets"
 	authzconfig "k8s.io/apiserver/pkg/apis/apiserver"
@@ -44,6 +45,7 @@ import (
 	webhookmetrics "k8s.io/apiserver/plugin/pkg/authorizer/webhook/metrics"
 	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/pkg/auth/authorizer/abac"
+	"k8s.io/kubernetes/pkg/auth/nodeidentifier"
 	"k8s.io/kubernetes/pkg/kubeapiserver/authorizer/modes"
 	"k8s.io/kubernetes/plugin/pkg/auth/authorizer/node"
 	"k8s.io/kubernetes/plugin/pkg/auth/authorizer/rbac"
@@ -222,6 +224,18 @@ func (r *reloadableAuthorizerResolver) newForConfig(authzConfig *authzconfig.Aut
 			authorizers = append(authorizers, union.NamedAuthorizer{
 				AuthorizerName: configuredAuthorizer.Name,
 				Authorizer:     authorizerfactory.NewPrivilegedGroups(user.SystemPrivilegedGroup),
+			})
+		case authzconfig.AuthorizerType(modes.ModeMinimumKubeletVersion):
+			// Add MinimumKubeletVerison authorizer, to block a node from being able to access most resources if it's not new enough.
+			// We must do so here instead of in pkg/apiserver because it relies on a node informer, which is not present in generic control planes.
+			authorizers = append(authorizers, union.NamedAuthorizer{
+				AuthorizerName: configuredAuthorizer.Name,
+				Authorizer: minimumkubeletversion.NewMinimumKubeletVersion(
+					GetMinimumKubeletVersion(),
+					nodeidentifier.NewDefaultNodeIdentifier(),
+					r.initialConfig.VersionedInformerFactory.Core().V1().Nodes().Informer(),
+					r.initialConfig.VersionedInformerFactory.Core().V1().Nodes().Lister(),
+				),
 			})
 		default:
 			return nil, nil, fmt.Errorf("unknown authorization mode %s specified", configuredAuthorizer.Type)
