ae2454c96c4d6a77d066297dbe28a0bf33b58653
Jacob See <5027680+jacobsee@users.noreply.github.com>
2026-05-04 16:58:11 -0700

    UPSTREAM: <carry>: add nil guard for KubeAPIReadyz event
    Could squash into UPSTREAM: <carry>: emit event when readyz goes true

--- pkg/controlplane/controller/kubernetesservice/controller.go
+++ pkg/controlplane/controller/kubernetesservice/controller.go
@@ -155,7 +155,9 @@ func (c *Controller) Run(ch <-chan struct{}) {
 		return
 	}
 
-	KubeAPIServerEmitEventFn(corev1.EventTypeWarning, "KubeAPIReadyz", "readyz=true")
+	if fn := KubeAPIServerEmitEventFn; fn != nil {
+		fn(corev1.EventTypeWarning, "KubeAPIReadyz", "readyz=true")
+	}
 
 	wait.NonSlidingUntil(func() {
 		// Service definition is not reconciled after first
